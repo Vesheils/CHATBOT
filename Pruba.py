bb2180e3aeab33a06a4e4ed323da5317fe1c289f
Vesheils <94071224+Vesheils@users.noreply.github.com>
2024-02-24 14:35:50 -0500

    Update Pruba.py

--- Pruba.py
+++ Pruba.py
@@ -1,42 +1,31 @@
 import streamlit as st
-from openai import OpenAI
+from openai import openai
 
-
-
-client = OpenAI(api_key = st.secrets.OpenAIAPI.openai_apikey)
-
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-  {"role": "system", "content": "Piensa como navegador web y dame resultados de busqueda y enlaces respecto a la busqueda"}
-]
-
-
-def communicate():
-    messages = st.session_state["messages"]
-    user_message = {"role": "user", "content": st.session_state["user_input"]}
-    messages.append(user_message)
-
-
-    response = client.chat.completions.create(
-      model="gpt-3.5-turbo",
-      messages=messages
-    )
-    bot_message = response.choices[0].message
-    messages.append(bot_message)
+class Pagina1:
+  def__init__(self):
+    self.client=OpenAI(api_key = secrets.OpenAIAPI.openai_api_key)
+      
+      prompt = "Simula ser Harvey Specter"
+      
+      if "trip_adviser_messages" not in st.session_state:
+      st.session_state["Harvey_Specter"] = ["role": "system", "content": prompt]
+    
+    def communicate():
+      messages = st.session_state["Harvey_Specter"]
+      user_message = {"role": "user", "content": st.session_state["user_input"]}
+      messages.append(user_message)
+      response = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages)
+      bot_message = response.choices[0].message
+      messages.append(bot_message)
 
 
     st.session_state["user_input"] = ""
+    st.title ("Desarrollador AI")
+    st.write ("Utilizando la API chatGPT, este chatbot ofrece capacidades conversacionales avanzadas.")
+  
+    user_input = st.text_input("por favor ingrese un mensaje aquí.", key = "user_input", on_change=communicate)
 
-
-st.title ("Desarrollador AI")
-st.write ("Utilizando la API chatGPT, este chatbot ofrece capacidades conversacionales avanzadas.")
-
-
-user_input = st.text_input("por favor ingrese un mensaje aquí.", key = "user_input", on_change=communicate)
-
-
-if st.session_state["messages"]:
+    if st.session_state["Harvey_Specter"]:
     messages = st.session_state["messages"]
 
 
@@ -47,3 +36,51 @@ if st.session_state["messages"]:
         else:
             st.write("🤖: " + message.content)
 
+
+class Pagina2:
+  def__init__(self):
+    self.client=OpenAI(api_key=secret_keys.openai_api_key)
+      
+      prompt2 = "Simula ser Mike Ross"
+      
+      if "trip_adviser_messages" not in st.session_state:
+      st.session_state["Mike_Ross"] = ["role": "system", "content": prompt2]
+    
+    def communicate():
+      messages = st.session_state["Mike_Ross"]
+      user_message = {"role": "user", "content": st.session_state["user_input"]}
+      messages.append(user_message)
+      response = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages)
+      bot_message = response.choices[0].message
+      messages.append(bot_message)
+
+
+    st.session_state["user_input"] = ""
+    st.title ("Desarrollador AI")
+    st.write ("Utilizando la API chatGPT, este chatbot ofrece capacidades conversacionales avanzadas.")
+  
+    user_input = st.text_input("por favor ingrese un mensaje aquí.", key = "user_input", on_change=communicate)
+
+    if st.session_state["Mike_Ross"]:
+    messages = st.session_state["messages"]
+
+
+    for message in reversed(messages[1:]):
+        if isinstance(message, dict):
+            speaker = "😎" if message["role"] == "user" else "🤖"
+            st.write (speaker + ": " + message["content"])
+        else:
+            st.write("🤖: " + message.content)
+
+def main():
+    siderbar = st.siderbar
+
+    page = siderbar.radio("Seleccione", ["PRUBA 1", "PRUBA 2"])
+
+    if page == "PRUBA1"
+      Pagina1()
+    else
+      Pagina2()
+
+if __name__ == "__main__":
+  main()
