4cd619bb8ff9fa3dccadb90d8b5821dcc3848111
Vesheils <94071224+Vesheils@users.noreply.github.com>
2024-02-24 10:54:16 -0500

    Add files via upload

--- Pruba.py
+++ Pruba.py
@@ -27,3 +27,22 @@ def communicate():
 
     st.session_state["user_input"] = ""
 
+
+st.title ("Desarrollador AI")
+st.write ("Utilizando la API chatGPT, este chatbot ofrece capacidades conversacionales avanzadas.")
+
+
+user_input = st.text_input("por favor ingrese un mensaje aquí.", key = "user_input", on_change=communicate)
+
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+
+    for message in reversed(messages[1:]):
+        if isinstance(message, dict):
+            speaker = "😎" if message["role"] == "user" else "🤖"
+            st.write (speaker + ": " + message["content"])
+        else:
+            st.write("🤖: " + message.content)
+
