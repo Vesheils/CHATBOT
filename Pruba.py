6b4e83ae845455664791ea89b8be120748950122
Vesheils <94071224+Vesheils@users.noreply.github.com>
2024-02-24 11:11:39 -0500

    Update Pruba.py

--- Pruba.py
+++ Pruba.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from openai import OpenAI
+import secret_keys
 
-client = OpenAI(api_key = openai_api_key)
+
+client = OpenAI(api_key = secret_keys.openai_api_key)
 
 
 if "messages" not in st.session_state:
